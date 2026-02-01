e5c6f1f9fc068e78ab33703aa38d8abf3d4d1bc7
Saad <mehmoodsaad347@gmail.com>
2026-02-01 21:43:56 +0530

    added google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,14 @@ import { Footer } from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/next";
+
 export const metadata = {
   title: "TechBlog – Mobile, Gadgets & Technology",
   description:
     "Latest mobile launches, best phones, gadgets, processors and tech explained.",
+  verification: {
+    google: "google77d08cbc9e81f5ef",
+  },
 };
 
 export default function RootLayout({
